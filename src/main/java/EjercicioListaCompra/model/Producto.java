bfab6d30b9dce84bad5f858e52305a7898b84477
Luis Quintano <149412452+Bigg69Cobra@users.noreply.github.com>
2023-12-20 19:58:10 +0100

    Add class Producto

--- src/main/java/EjercicioListaCompra/model/Producto.java
+++ src/main/java/EjercicioListaCompra/model/Producto.java
@@ -1,6 +1,59 @@
 package EjercicioListaCompra.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Producto {
-    String cantidad;
-    String nombre;
+    @Id  //siempre tiene que estar encima del atributo que quieras que sea el id (Primary Key).
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  //para que se auto incremente solo.
+    @Column(name = "id")  //para que la columna de la tabla se llame como le digas.
+    private int id;
+
+    @Column(name = "cantidad")  //para que la columna de la tabla se llame como le digas.
+    private String cantidad;
+
+    @Column(name = "nombre")  //para que la columna de la tabla se llame como le digas.
+    private String nombre;
+
+
+    //constructor
+    public Producto() {
+    }
+
+    public Producto(String cantidad, String nombre) {
+        this.cantidad = cantidad;
+        this.nombre = nombre;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "cantidad='" + cantidad + '\'' +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+
+    //getters and setters
+    public int getId() {
+        return id;
+    }
+    public String getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(String cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
 }
