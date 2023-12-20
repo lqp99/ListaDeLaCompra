99b3e7985915117a3dc4b6c337696d224e40367d
Luis Quintano <149412452+Bigg69Cobra@users.noreply.github.com>
2023-12-20 20:04:34 +0100

    Add class ProductoDAO

--- src/main/java/EjercicioListaCompra/interfaces/ProductoDAO.java
+++ src/main/java/EjercicioListaCompra/interfaces/ProductoDAO.java
@@ -1,4 +1,15 @@
 package EjercicioListaCompra.interfaces;
 
+import EjercicioListaCompra.model.Producto;
+
 public interface ProductoDAO {
+    public void listarTodosLosProductos();
+
+    public void restarXProducto(Producto p);
+
+    public void mostrarProducto(String nombre);
+
+    public void addXProducto(Producto p);
+
+    public void exitProgram();
 }
